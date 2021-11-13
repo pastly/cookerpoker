762cb0f37e890916d7e204a1fb713c02dba4f005
deedasmi <deedasmi@protonmail.com>
2021-11-13 02:01:04 -0600

    Forgot to save again

--- poker-server/src/account.rs
+++ poker-server/src/account.rs
@@ -3,8 +3,6 @@ use rocket::request::{self, Outcome, Request, FromRequest};
 use crate::database::{DbConn, models::{SettledAccount, Account}};
 use diesel::prelude::*;
 
-pub struct ApiKey<'r>(&'r str);
-
 async fn api_to_account(db: DbConn, key: String) -> Result<Account, ApiKeyError> {
     use crate::database::schema::settled_accounts::dsl::settled_accounts;
     use crate::database::schema::accounts::dsl::{accounts, api_key};
@@ -18,25 +16,6 @@ async fn api_to_account(db: DbConn, key: String) -> Result<Account, ApiKeyError>
 pub enum ApiKeyError {
     Missing,
     Invalid,
-}
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for ApiKey<'r> {
-    type Error = ApiKeyError;
-
-    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        /// Returns true if `key` is a valid API key string.
-        fn is_valid(key: &str) -> bool {
-            // TODO
-            key.len()>10
-        }
-
-        match req.headers().get_one("x-api-key") {
-            None => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
-            Some(key) if is_valid(key) => Outcome::Success(ApiKey(key)),
-            Some(_) => Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid)),
-        }
-    }
 }
 
 pub struct User(pub Account);
