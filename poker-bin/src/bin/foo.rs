437e461ba4d96ff4a08cb48a080e0db0c74be3d2
Matt Traudt <sirmatt@ksu.edu>
2021-12-06 11:17:06 -0500

    Fix forgotten Deck::new() -> Deck::default()

--- poker-bin/src/bin/foo.rs
+++ poker-bin/src/bin/foo.rs
@@ -5,7 +5,7 @@ fn main() {
     let n = 7;
     let reps = 1;
     for rep in 0..reps {
-        let mut d = Deck::new();
+        let mut d = Deck::default();
         let cards: Vec<_> = (0..n).map(|_| d.draw().unwrap()).collect();
         for item in best_of_cards(&cards) {
             println!("{} {}", rep, item);
