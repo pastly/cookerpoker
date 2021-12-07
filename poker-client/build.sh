8b47883cd5ffa28e0afbf3771b36e3a79c7d5d57
Matt Traudt <sirmatt@ksu.edu>
2021-12-06 21:04:17 -0500

    Initial toy wasm code

---
+++ poker-client/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu
+
+if [[ "$#" != "1" ]] || [[ "$1" != "--release" ]]; then
+	MODE="--dev"
+else
+	MODE="--release"
+fi
+echo "Compiling with $MODE"
+for CRATE_DIR in .; do
+    cd $CRATE_DIR
+    cargo fmt
+    cargo clippy
+    cargo check
+    wasm-pack -v build $MODE --target web
+    cd -
+done
