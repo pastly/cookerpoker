af73538f3666e7c6d47426db22a6e4cc50a30b17
Matt Traudt <sirmatt@ksu.edu>
2022-01-13 22:06:24 -0500

    WIP rework logging in.
    Done:
    
    - Use an actual form.
    - Validate string given is in the shape of an API key.
    - "New" fn to lookup an API key in DB and return Account if found.
    - Set private cookie (named "session" not "api-key") with Account::id.
    
    Not done:
    
    - index and index_anon routes. The fallback from index to index_anon
      isn't there yet. Need Outcome::Forward(???) when code fails to find
    the "session" cookie and extract the Account::id and get an Account.
    - clean up old login/index code
    - something basic that will show the user they're logged in or not. Like
      showing their name on index vs "anonymous" on index_anon

--- poker-server/src/endpoints/forms/accounts.rs
+++ poker-server/src/endpoints/forms/accounts.rs
@@ -1,3 +1,5 @@
+use crate::endpoints::logic::account::ApiKey;
+
 #[derive(FromForm)]
 pub struct ModSettled {
     pub change: i32,
@@ -8,4 +10,9 @@ pub struct ModSettled {
 pub struct NewAccount {
     pub account_name: String,
     pub is_admin: bool,
+}
+
+#[derive(FromForm, Debug)]
+pub struct LoginForm {
+    pub api_key: ApiKey,
 }
