4838b520e3ac73438f9520e97f0a46e49d241cdc
Matt Traudt <sirmatt@ksu.edu>
2022-01-20 19:53:12 -0500

    Move wasm demo stuff to new index

--- poker-server/src/endpoints/index.rs
+++ poker-server/src/endpoints/index.rs
@@ -20,14 +20,3 @@ fn index_anon() -> Template {
     let c = Context::new();
     Template::render("index", &c.into_json())
 }
-//#[get("/", rank = 2)]
-//fn index() -> Redirect {
-//    Redirect::to("/index.html")
-//}
-//
-//#[get("/")]
-//fn index_logged_in(u: User) -> Template {
-//    let mut c = Context::new();
-//    c.insert("account", &(*u));
-//    Template::render("logged_in", &c.into_json())
-//}
